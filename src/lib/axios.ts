5f074dbd6548882272f3a75de6dd0d4e354183e4
Dust07 <tienphat0707@gmail.com>
2024-07-28 17:51:59 +0700

    Fix - Fix build error

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -14,7 +14,6 @@ export const API_TIMEOUT_DURATION = 30000;
 export const axiosClient = axios.create({
   timeout: API_TIMEOUT_DURATION,
   headers: {
-    "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   },
 });
