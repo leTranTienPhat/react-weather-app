8f882dbd84e37e54897128a78898c9d0c97980c9
Dust07 <tienphat0707@gmail.com>
2024-07-28 20:49:12 +0700

    Update: Optimize

--- src/components/common/GlassWrapper.tsx
+++ src/components/common/GlassWrapper.tsx
@@ -6,7 +6,7 @@ type IProps = {
 };
 
 const GlassWrapper = ({ children, className }: IProps) => {
-  return <div className={cn("w-full bg-glass/20 p-10 rounded-lg", className)}>{children}</div>;
+  return <div className={cn("w-full bg-glass/20 p-4 sm:p-10 rounded-lg", className)}>{children}</div>;
 };
 
 export default GlassWrapper;
